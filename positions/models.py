5104d5e0edb95dd8323b83e90a5311b195398f0c
Haohong Xu <weidai1122@gmail.com>
2018-10-02 07:55:32 -0700

    Add models

--- positions/models.py
+++ positions/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    symbol = models.CharField(max_length=10)
+    quantity = models.FloatField()
+    purchase_price = models.FloatField()
+
+    @classmethod
+    def build_from_dict(cls, data):
+        """
+        Build instance from dict
+        """
+
+        return cls(
+            symbol=data['symbol'],
+            quantity=float(data['quantity']),
+            purchase_price=float(data['purchase_price']),
+        )
+
+
+class Price(models.Model):
+    symbol = models.CharField(max_length=10)
+    price = models.FloatField()
+
+    @classmethod
+    def build_from_dict(cls, data):
+        """
+        Build instance from dict
+        """
+
+        return cls(
+            symbol=data['symbol'],
+            price=float(data['price']),
+        )
+
+
+class Trade(models.Model):
+    TRADE_SIDE_SELL = 'sell'
+    TRADE_SIDE_BUY = 'buy'
+
+    TRADE_SIDES = (
+        (TRADE_SIDE_SELL, 'Sell'),
+        (TRADE_SIDE_BUY, 'Buy'),
+    )
+
+    side = models.CharField(choices=TRADE_SIDES, max_length=10)
+    symbol = models.CharField(max_length=10)
+    quantity = models.FloatField()
+    price = models.FloatField()
+
+    @classmethod
+    def build_from_dict(cls, data):
+        """
+        Build instance from dict
+        """
+
+        return cls(
+            side=data['side'],
+            symbol=data['symbol'],
+            quantity=float(data['quantity']),
+            price=float(data['price']),
+        )
