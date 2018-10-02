310b75782b847385fbc31565d37167e7532fbe2c
Haohong Xu <weidai1122@gmail.com>
2018-10-02 08:12:20 -0700

    Add positions query(Part 1)

--- drip_graphql/settings.py
+++ drip_graphql/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'graphene_django',
+
+    'positions.apps.PositionsConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+GRAPHENE = {
+    'SCHEMA': 'drip_graphql.schema.schema',
+}
